197875c4040e7a09344b75de571fbac0621cf7a6
amedeopro <amedeopro@me.com>
2019-02-14 23:11:27 +0100

    esercizio completato

--- magazzino.php
+++ magazzino.php
@@ -18,3 +18,23 @@
      <li>Quantita rimanente: <?php echo $cartaA4->rimanenze(); ?></li>
    </ul>
  </div>
+
+
+ <?php $dixan = new Prodotto("casalinghi")  ?>
+ <?php
+ $dixan->nomeProdotto = "Dixan Piatti";
+ $dixan->brandProdotto = "Henkel";
+ $dixan->quantita = 135;
+ $dixan->quantitaVendute = 36;
+  ?>
+
+
+ <div class="prodotto">
+   <h1><?php echo $dixan->categoria; ?></h1>
+   <ul>
+     <li>Nome Prodotto: <?php echo $dixan->nomeProdotto; ?></li>
+     <li>Marca: <?php echo $dixan->brandProdotto; ?></li>
+     <li>Quantita acquistata: <?php echo $dixan->quantita; ?></li>
+     <li>Quantita rimanente: <?php echo $dixan->rimanenze(); ?></li>
+   </ul>
+ </div>
